87f99353c078a99dd044ba5ee61ecfb7cb35c74e
Sree Kodem <skodem@uncc.edu>
2018-03-15 15:22:11 -0400

    Update app.js

--- server/app.js
+++ server/app.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Welcome to Grocery Service APIs.')
+  res.send('Welcome.')
 })
 
 app.listen(3000, function () {
-  console.log('Grocery Web app service listening on port 3000!')
+  console.log('Web app service listening on port 3000!')
 })
